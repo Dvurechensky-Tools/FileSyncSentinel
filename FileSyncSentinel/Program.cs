195778e8a8f2f201f45b6675c610ed42b1ab43d7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-05 07:22:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-05

--- FileSyncSentinel/Program.cs
+++ FileSyncSentinel/Program.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 мая 2026 07:14:36
- * Version: 1.0.187
+ * Last Updated: 05 мая 2026 07:22:47
+ * Version: 1.0.188
  */
 
 namespace FileSyncSentinel
