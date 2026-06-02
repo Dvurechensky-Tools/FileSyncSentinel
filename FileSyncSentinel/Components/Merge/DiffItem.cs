a0cffffe6053eac91322e8ed43f8aef8dd96d631
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-02 07:14:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-02

--- FileSyncSentinel/Components/Merge/DiffItem.cs
+++ FileSyncSentinel/Components/Merge/DiffItem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 июня 2026 15:08:17
- * Version: 1.0.214
+ * Last Updated: 02 июня 2026 07:14:34
+ * Version: 1.0.215
  */
 
 using DiffPlex.DiffBuilder.Model;
