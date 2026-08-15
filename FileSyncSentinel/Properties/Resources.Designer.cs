6f5fd8c718e48afd99cdcb8fc7d635fa6aea942d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-15 07:15:43 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-15

--- FileSyncSentinel/Properties/Resources.Designer.cs
+++ FileSyncSentinel/Properties/Resources.Designer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 августа 2026 08:38:51
- * Version: 1.0.287
+ * Last Updated: 15 августа 2026 07:15:40
+ * Version: 1.0.288
  */
 
 //------------------------------------------------------------------------------
