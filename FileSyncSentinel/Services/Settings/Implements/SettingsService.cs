cc318e2371ad850f039cddf8f1aa9dcae91bfd38
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-01 08:54:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-01

--- FileSyncSentinel/Services/Settings/Implements/SettingsService.cs
+++ FileSyncSentinel/Services/Settings/Implements/SettingsService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 августа 2026 07:10:02
- * Version: 1.0.304
+ * Last Updated: 01 сентября 2026 08:54:33
+ * Version: 1.0.305
  */
 
 using System.Diagnostics;
