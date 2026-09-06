9621aea1e4495ac217dde70c1a1bd1b1c91cbc40
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-06 11:14:38 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-06

--- FileSyncSentinel/Services/Merge/IMergeService.cs
+++ FileSyncSentinel/Services/Merge/IMergeService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 сентября 2026 08:58:28
- * Version: 1.0.309
+ * Last Updated: 06 сентября 2026 11:14:35
+ * Version: 1.0.310
  */
 
 using FileSyncSentinel.Components;
