971b73a78c59e8e15fcbd5a3a71fc5a1fa4090db
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-14 09:57:09 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-14

--- FileSyncSentinel/Components/Merge/ChangeListFilter.cs
+++ FileSyncSentinel/Components/Merge/ChangeListFilter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 сентября 2026 06:58:09
- * Version: 1.0.317
+ * Last Updated: 14 сентября 2026 09:57:06
+ * Version: 1.0.318
  */
 
 namespace FileSyncSentinel.Components
