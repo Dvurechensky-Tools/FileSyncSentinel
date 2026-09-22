1193d5a07630f7936d73b0c2384fb7966b92f1df
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-22 09:24:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-22

--- FileSyncSentinel/Components/Merge/MergeConfigData.cs
+++ FileSyncSentinel/Components/Merge/MergeConfigData.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 сентября 2026 09:41:10
- * Version: 1.0.325
+ * Last Updated: 22 сентября 2026 09:24:49
+ * Version: 1.0.326
  */
 
 using Newtonsoft.Json;
